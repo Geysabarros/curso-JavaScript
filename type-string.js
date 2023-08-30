14dd55806ced4b26183f6e3154a1890ae02374fb
Geysa <geysabarros@yahoo.com.br>
2023-08-30 16:47:43 -0300

    Exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,25 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Geysa"
+
+//concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string ou template literal
+
+//para saber mais
+
+const cifrao = '\u0024';
+const aMaiusculo = '\u0041';
+const tique = '\u2705';
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
